b37cba15c1998409770a60e6c4acd4bd94d0ca2c
Omar Zeidan <omar.zeidan@itp.com>
2014-08-14 19:57:39 +0300

    event page

--- TimeOutProject/js/app.js
+++ TimeOutProject/js/app.js
@@ -20,4 +20,23 @@ $(document).ready(function() {
     $(".next").click(function(){
     	$('.StepsContent').slickNext();
     });
+
+
+ $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+});
+                           
+
 });
